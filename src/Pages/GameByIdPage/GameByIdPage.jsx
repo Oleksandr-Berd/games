2fb4319dbbb038dcf84568be2aaabc8c080c5b83
Oleksandr-Berd <alex.berd86@gmail.com>
2023-05-24 14:14:46 +0300

    styling gameidpage

--- src/Pages/GameByIdPage/GameByIdPage.jsx
+++ src/Pages/GameByIdPage/GameByIdPage.jsx
@@ -2,42 +2,52 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { getGameById } from "../../utilities/helpers";
-import * as SC from "./Styles"
+import * as SC from "./Styles";
 
 const GameByIdPage = () => {
-     const { id } = useParams();
-     const [game, setGame] = useState({});
+  const { id } = useParams();
+  const [game, setGame] = useState({});
 
-    const { background_image, name, description, background_image_additional } =
-      game;
-    
-      useEffect(() => {
-          getGameById(id).then(setGame);
-          
-         
-      }, [id]);
-    const symbol = "<p>";
-    const symbolToDelete = "</p>";
-   
+  const {
+    background_image,
+    name,
+    description,
+    background_image_additional,
+    genres,
+  } = game;
 
+  useEffect(() => {
+    getGameById(id).then(setGame);
+  }, [id]);
+  const symbol = "<p>";
+  const symbolToDelete = "</p>";
 
-    let modifiedStr = description
-      ?.replace(new RegExp(symbol, "g"), "")
-      .replace(new RegExp(symbolToDelete, "g"), "");
-    
-    
-    return (
-      <SC.Container>
-        <SC.Image src={background_image} alt={name} />
-        <SC.TitleContainer>
+  let modifiedStr = description
+    ?.replace(new RegExp(symbol, "g"), "")
+    .replace(new RegExp(symbolToDelete, "g"), "");
+
+  return (
+    <SC.Container>
+      <SC.Image src={background_image} alt={name} />
+      <SC.TitleContainer>
+        <SC.LeftSideContainer>
           <SC.Title>{name}</SC.Title>
-          <SC.ImageAdd src={background_image_additional} alt={name} />
-        </SC.TitleContainer>
-        <SC.Description>{modifiedStr}</SC.Description>
-      </SC.Container>
-    );
-    
-
- }
+          <SC.GenresTitle>Genres:</SC.GenresTitle>
+          <SC.GenresList>
+            {genres &&
+              genres.map(({ name, image_background }) => (
+                <SC.GenresItem key={name}>
+                  <SC.GenresImage src={image_background} alt={name} width={36} />
+                  <h4>{name}</h4>
+                </SC.GenresItem>
+              ))}
+          </SC.GenresList>
+        </SC.LeftSideContainer>
+        <SC.ImageAdd src={background_image_additional} alt={name} />
+      </SC.TitleContainer>
+      <SC.Description>{modifiedStr}</SC.Description>
+    </SC.Container>
+  );
+};
 
 export default GameByIdPage;
--- src/Pages/GameByIdPage/Styles.js
+++ src/Pages/GameByIdPage/Styles.js
@@ -13,14 +13,22 @@ border-radius: ${props => props.theme.radius[2]+"px"};
 `
 
 export const TitleContainer = styled.div`
-display: flex;
-flex-direction: row;
+  display: ${(props) => props.theme.flex.display};
+  flex-direction: ${(props) => props.theme.flex.flexDirection[0]};
+  justify-content: ${(props) => props.theme.flex.justifyContent[1]};
+  padding-left: ${(props) => props.theme.spacing[5] + "px"};
+  padding-right: ${(props) => props.theme.spacing[5] + "px"};
+  margin-bottom: ${(props) => props.theme.spacing[6] + "px"};
+`;
+
+export const LeftSideContainer = styled.div`
+text-align: ${props => props.theme.listText.textAlignLeft};
 `
 
 export const ImageAdd = styled.img`
-  max-width: ${(props) => props.theme.percentage[7]};
+  max-width: ${(props) => props.theme.percentage[6]};
 
-  border-radius: ${(props) => props.theme.radius[2] + "px"};
+  border-radius: ${(props) => props.theme.radius[4] + "px"};
 `;
 
 export const Title = styled.h2`
@@ -29,6 +37,35 @@ font-family: ${props => props.theme.listText.fontFamilyTitle};
 font-size: ${props => props.theme.listText.fontSizeXM};
 font-style: ${props => props.theme.listText.fontStyleItalic};
 `
-export const Description = styled.article`
+
+export const GenresList = styled.ul`
+  display: ${(props) => props.theme.grid.display};
+  grid-template-columns: repeat(2, 1fr);
+  gap: ${(props) => props.theme.spacing[4] + "px"};
+
   color: ${(props) => props.theme.color.text};
+  font-family: ${(props) => props.theme.listText.fontFamilyText};
+`;
+
+export const GenresTitle = styled.h3`
+  color: ${(props) => props.theme.color.text};
+  font-family: ${(props) => props.theme.listText.fontFamilyText};
+  font-weight: ${(props) => props.theme.listText.fontWeightBold};
+`;
+
+export const GenresItem = styled.li`
+display: ${props => props.theme.flex.display};
+`
+
+export const GenresImage = styled.img`
+margin-right: ${props => props.theme.spacing[4]+"px"};
+`
+
+export const Description = styled.article`
+max-width: ${props => props.theme.percentage[8]};
+margin-left: auto;
+margin-right: auto;
+
+  color: ${(props) => props.theme.color.text};
+  font-family: ${(props) => props.theme.listText.fontFamilyText};
 `;
